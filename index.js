4a4e692e19f828331837af08ddbef032be9da106
Moises <gmoises926@gmail.com>
2021-01-07 20:27:36 -0600

    Animaciones de logo y presentación

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 /** Calcula automáticamente la edad */
-function getAge(){
+function getAge() {
     let now = Date.now()
-    let birth = new Date(2000,1,5)
-    let milisecByYear = 365*24*60*60*1000
-    return parseInt((now-birth)/milisecByYear)
+    let birth = new Date(2000, 1, 5)
+    let milisecByYear = 365 * 24 * 60 * 60 * 1000
+    return parseInt((now - birth) / milisecByYear)
 }
 
-function meltAnimation(){
-    setTimeout(
-        () => {
-            var disappearObjs = document.querySelectorAll(".disappear");
-            disappearObjs.forEach((obj)=>{
-                obj.style.opacity=0;
-            })
-        },
-        300
-    )
-    
-    setTimeout(() => {
-        var appearObjs = document.querySelectorAll(".appear");
-        appearObjs.forEach((obj) =>{
-            obj.style.visibility = "visible"
-            obj.style.opacity = 1;
-        })
-    }, 1700);
+const animateCSS = (element, animation, prefix = 'animate__', delay = 'null') => {
+    // We create a Promise and return it
+    return new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = document.querySelector(element);
 
+        node.classList.add(`${prefix}animated`, animationName, delay);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
 }
+
+window.addEventListener('load',  () => {
+    document.getElementById("age").innerHTML = `${getAge()} años`;
+
+    animateCSS("#MG", "lightSpeedInLeft").then((message) => {
+        animateCSS("#MG", "bounceOut","animate__","animate__delay-1s").then((res) => {
+            document.getElementById("MG").style.visibility = "hidden";
+        });
+    });
+
+    animateCSS("#brandShape","lightSpeedInRight").then((message) =>{
+        animateCSS("#brandShape","bounceOut","animate__","animate__delay-1s").then((res) => {
+            document.getElementById("brandShape").style.visibility = "hidden";
+            document.getElementById("principalSection").style.visibility = "visible";
+            animateCSS("#principalSection","fadeInDownBig");
+        });
+    });
+});
